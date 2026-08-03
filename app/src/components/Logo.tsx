ae1f7244d149c28cbbf5a0e6285849d7afdcc1fe
andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>
2026-08-03 15:26:51 +0100

    feat: add community light and dark logos & update logo and footer depending on env var
    Signed-off-by: andyrodrigues30 <andrea.melissa.rodrigues@gmail.com>

---
+++ app/src/components/Logo.tsx
@@ -0,0 +1,30 @@
+import { Link } from "@tanstack/react-router";
+import { useTheme } from "@/lib/themeProvider";
+
+const BLUELEARN_OFFICIAL = import.meta.env.VITE_BLUELEARN_OFFICIAL;
+
+export const Logo = () => {
+  const { theme } = useTheme();
+
+  if (BLUELEARN_OFFICIAL == "true") {
+    return theme == "light" ? (
+      <Link to="/" className="flex items-center gap-3">
+        <img src={`/assets/logo/og/logo-og-grad.svg`} className="h-8" />
+      </Link>
+    ) : (
+      <Link to="/" className="flex items-center gap-3">
+        <img src={`/assets/logo/og/logo-og-bb.svg`} className="h-8" />
+      </Link>
+    );
+  } else {
+    return theme == "light" ? (
+      <Link to="/" className="flex items-center gap-3">
+        <img src={`/assets/logo/ce/logo-ce-dark.svg`} className="h-8" />
+      </Link>
+    ) : (
+      <Link to="/" className="flex items-center gap-3">
+        <img src={`/assets/logo/ce/logo-ce-light.svg`} className="h-8" />
+      </Link>
+    );
+  }
+};
