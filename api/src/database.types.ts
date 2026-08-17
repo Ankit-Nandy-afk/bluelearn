42c56dd101236d891b6c19858c4e9975840abd86
Tonycs <lam.tony540@gmail.com>
2026-08-17 17:53:48 -0400

    chore(db): regen supabase types
    Signed-off-by: Tonycs <lam.tony540@gmail.com>

--- api/src/database.types.ts
+++ api/src/database.types.ts
@@ -7,30 +7,10 @@ export type Json =
   | Json[]
 
 export type Database = {
-  graphql_public: {
-    Tables: {
-      [_ in never]: never
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      graphql: {
-        Args: {
-          extensions?: Json
-          operationName?: string
-          query?: string
-          variables?: Json
-        }
-        Returns: Json
-      }
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
+  // Allows to automatically instantiate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: "14.5"
   }
   public: {
     Tables: {
@@ -1430,9 +1410,6 @@ export type CompositeTypes<
     : never
 
 export const Constants = {
-  graphql_public: {
-    Enums: {},
-  },
   public: {
     Enums: {
       app_role: ["verifier", "moderator", "curator", "admin", "official"],
@@ -1474,4 +1451,3 @@ export const Constants = {
     },
   },
 } as const
-
